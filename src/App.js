7ee7226d1d054cd2b39f1055de7468081012ba0c
Amit Kumar Sahu <aksahu1@q3infotech.net>
2024-04-22 18:26:38 +0530

    updated css for spacing variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.scss';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome in the project</h1>
+        <h1>Welcome!</h1>
       </header>
     </div>
   );
