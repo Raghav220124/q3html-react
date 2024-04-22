7d6fcd048e3f1f6c8ab35979a258c9574e00d7c7
Vibhav Ranjan <Vibhav.Ranjan@q3infotech.net>
2024-04-22 18:38:39 +0530

    font added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.scss';
+import Fonts from './Components/Fonts';
 function App() {
   return (
     <div className="App">
@@ -8,6 +8,7 @@ function App() {
         <h2>FOnt changes</h2>
 
       </header>
+      <Fonts />
     </div>
   );
 }
---
+++ src/Components/Fonts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Fonts() {
+    return (
+      <div>
+        <h2 className='roboto_light'> Font Family Roboto Light</h2>
+        <h2 className='roboto_regular'> Font Family Roboto Regular</h2>
+        <h2 className='roboto_medium'> Font Family Roboto Mediun</h2>
+        <h2 className='roboto_bold'> Font Family Roboto Bold</h2>
+      </div>
+    );
+  }
+  
+  export default Fonts;
